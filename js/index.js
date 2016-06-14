c1b70086eb821a1d3d76695a323bc94500721bc2
Z-Leila <1595466043@qq.com>
2016-06-14 17:30:23 +0800

    dangdangwang

---
+++ js/index.js
@@ -0,0 +1,19 @@
+$(function(){
+    var swiper = new Swiper('.swiper-container', {
+        pagination: '.swiper-pagination',
+        paginationClickable: true,
+        spaceBetween: 30,
+        centeredSlides: true,
+        autoplay: 2500,
+        autoplayDisableOnInteraction: false
+    });
+
+     var swiper2 = new Swiper('.swiper-container2', {
+        pagination: '.swiper-pagination',
+        slidesPerView: 3,
+        paginationClickable: true,
+        // spaceBetween: 30
+    });
+
+})
+    
